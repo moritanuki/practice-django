89e527ee6cc341540f95c58ccbc94ac3bfb9defb
moritanuki <82251856+moritanuki@users.noreply.github.com>
2021-12-26 12:33:04 +0900

    created view templates

--- practice_app/urls.py
+++ practice_app/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from .views import HelloWorldView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('helloworld/', HelloWorldView.as_view())
 ]
